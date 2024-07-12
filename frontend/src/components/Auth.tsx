4a1a9078ccda9358008edb9b3314ff266375f04d
WebDclassified <vikaschaudhary0720@gmail.com>
2024-07-12 10:21:15 +0530

    more update 2.0

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -40,19 +40,19 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
                     </div>
                 </div>
                 <div className="pt-8">
-                    {type === "signup" ? <LabelledInput label="Name" placeholder="Harkirat Singh..." onChange={(e) => {
+                    {type === "signup" ? <LabelledInput label="Name" placeholder="John Doe..." onChange={(e) => {
                         setPostInputs({
                             ...postInputs,
                             name: e.target.value
                         })
                     }} /> : null}
-                    <LabelledInput label="Username" placeholder="harkirat@gmail.com" onChange={(e) => {
+                    <LabelledInput label="Username" placeholder="johndoe@gmail.com" onChange={(e) => {
                         setPostInputs({
                             ...postInputs,
                             username: e.target.value
                         })
                     }} />
-                    <LabelledInput label="Password" type={"password"} placeholder="123456" onChange={(e) => {
+                    <LabelledInput label="Password" type={"password"} placeholder="Password" onChange={(e) => {
                         setPostInputs({
                             ...postInputs,
                             password: e.target.value
